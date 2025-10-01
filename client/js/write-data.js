dcfa4e806758e00360d6a2ee4f18c24c7364e037
bigbither <144506558+bigbither@users.noreply.github.com>
2025-10-01 16:55:33 -0400

    c

--- client/js/write-data.js
+++ client/js/write-data.js
@@ -1,10 +1,12 @@
 const submitButton = document.getElementById("submit-button");
 const clearButton = document.getElementById("clear-button");
 
-submitButton.addEventListener('click', function() {
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
     alert("Submit button was pressed");
 });
 
-clearButton.addEventListener('click', function () {
+clearButton.addEventListener('click', function (event) {
+    event.preventDefault();
     document.getElementById("enter-data").reset();
-});
+}); 
